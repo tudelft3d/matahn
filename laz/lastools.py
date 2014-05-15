57a67f31b984e229f061684f23f9f909ec8c48f5
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-05-15 16:15:09 +0200

    watch out: wine is in the list of subprocess for me
    can’t be bothered to compile lastools

--- laz/lastools.py
+++ laz/lastools.py
@@ -1,7 +1,9 @@
 import subprocess32 as subprocess
 
-LASINFO_BINARY = '/usr/local/bin/lasinfo_lastools'
-LASMERGE_BINARY = '/usr/local/bin/lasmerge_lastools'
+# LASINFO_BINARY = '/usr/local/bin/lasinfo_lastools'
+# LASMERGE_BINARY = '/usr/local/bin/lasmerge_lastools'
+LASINFO_BINARY  = '/Users/hugo/software/lastools/bin/lasinfo'
+LASMERGE_BINARY = 'lasmerge'
 
 def lasmerge(filenames, x_min, y_min, x_max, y_max, outname):
 	q = [LASMERGE_BINARY, '-inside', x_min, y_min, x_max, y_max, '-olaz', outname, '-i'] + filenames
@@ -9,6 +11,7 @@ def lasmerge(filenames, x_min, y_min, x_max, y_max, outname):
 	return outname
 
 def lasinfotxt(filename):
-	subprocess.call([LASINFO_BINARY, filename, '-otxt'])
+  # subprocess.call([LASINFO_BINARY, filename, '-otxt'])
+	subprocess.call(['wine', LASINFO_BINARY, filename, '-otxt'])
 	return filename[:-3]+'txt'
 
