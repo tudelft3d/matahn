e29cd3b93042f308b809526daf64c183f74facf9
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-05-21 15:40:53 +0200

    to run on a real server, run should,t be called

--- runserver.py
+++ runserver.py
@@ -10,6 +10,7 @@ application = DispatcherMiddleware( frontend_app, {
     '/matahn':     matahn_app
 })
 
-run_simple('localhost', 5000, application, use_reloader=True)
+if __name__ == '__main__':
+    run_simple('localhost', 5000, application, use_reloader=True)
 
 
