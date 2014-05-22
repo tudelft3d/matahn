e2f3eab337172ed35ea7d20f8745a7e0bb27679b
Ravi Peters <ravipeters@3dsm.bk.tudelft.nl>
2014-05-22 13:55:37 +0200

    this works

--- matahn/__init__.py
+++ matahn/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 
+class default_settings(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://matahn:matahn@localhost/matahn'
+    RESULTS_FOLDER 	= '/data/matahn/results/'
+    BLADINDEX_JSON = '/var/www/webahn/tiles.json'
+    LASINFO_BINARY = '/usr/local/bin/lasinfo'
+    LASMERGE_BINARY = '/usr/local/bin/lasmerge'
+    CELERY_BROKER_URL = 'redis://localhost:6379/0',
+    CELERY_RESULT_BACKEND = 'db+'+SQLALCHEMY_DATABASE_URI
+
 app = Flask(__name__, static_url_path='')
-app.config.from_object('matahn.default_settings')
-app.config.from_envvar('MATAHN_SETTINGS')
+app.config.from_object(default_settings)
+# app.config.from_envvar('MATAHN_SETTINGS')
 
 import matahn.views
 import matahn.models
