ef42b1c0eaea4e647ccd8ee09d9c617b00d1ccfc
Ravi Peters <ravipeters@3dsm.bk.tudelft.nl>
2015-02-10 10:44:10 +0100

    update due to moving of 3dsm page

--- runserver.py
+++ runserver.py
@@ -5,7 +5,9 @@ from werkzeug.wsgi import DispatcherMiddleware
 
 from matahn import app as matahn_app
 
-application = DispatcherMiddleware( matahn_app )
+application = DispatcherMiddleware( None, {
+    '/matahn':     matahn_app
+} )
 
 if __name__ == '__main__':
     run_simple('localhost', 5000, application, use_reloader=True)
